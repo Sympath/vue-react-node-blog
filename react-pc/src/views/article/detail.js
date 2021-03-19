59223b864ee857f65268fb1a023cde2a3d4cea6f
fujinting358599 <2239216274@qq.com>
2021-03-19 10:55:42 +0800

    feat: react自定义评论组件替换为ant-design组件

--- react-pc/src/views/article/detail.js
+++ react-pc/src/views/article/detail.js
@@ -1,10 +1,10 @@
 import React from "react";
-import {
-  LikeOutlined
-} from "@ant-design/icons";
+import { Input, Button, Avatar, Comment } from "antd";
+import { LikeOutlined } from "@ant-design/icons";
 import "../../assets/style/pages/article-detail.scss";
 import "mavon-editor/dist/css/index.css";
-import { getArticle,postArticleRead,postArticleLike} from "../../api/api";
+import { getArticle, postArticleRead, postArticleLike } from "../../api/api";
+const { TextArea } = Input;
 
 export default class ArticleDetail extends React.Component {
   constructor(props) {
@@ -33,13 +33,51 @@ export default class ArticleDetail extends React.Component {
             />
           </div>
           <div className="like-bar">
-            <div onClick={()=>{
-              this._like()
-            }}>
-            <LikeOutlined className="like-icon"/>点赞({this.state.detail.like})
+            <div
+              onClick={() => {
+                this._like();
+              }}
+            >
+              <LikeOutlined className="like-icon" />
+              点赞({this.state.detail.like})
             </div>
-          
           </div>
+          <div className="textarea-bar">
+            <div className="textarea">
+              <h2>评论一下</h2>
+              <TextArea
+                className="comment-textarea"
+                placeholder="说点什么吧！"
+                rows={5}
+              />
+              <Button className="comment-btn" type="primary">
+                发 送
+              </Button>
+            </div>
+          </div>
+          <div className="comments">
+            <div className="comment">
+              {/* 评论 */}
+
+              <Comment
+                actions={[<span key="comment-nested-reply-to">回复</span>]}
+                author={<a>付金廷</a>}
+                avatar={
+                  <Avatar
+                    src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
+                    alt="Han Solo"
+                  />
+                }
+                content={
+                  <p>
+                    我们提供一系列的设计原则，实用的模式和高质量的设计资源(素描和Axure)。
+                  </p>
+                }
+              >
+              </Comment>
+            </div>
+          </div>
+
           <div className="other"></div>
         </div>
       </>
@@ -47,17 +85,12 @@ export default class ArticleDetail extends React.Component {
   }
 
   componentDidMount() {
-     // 获取文章详情
-    this.init()
+    // 获取文章详情
+    this.init();
     // 获取阅读量
-    this.postRead()
-    
-
-   
-      
-    
+    this.postRead();
   }
-  postRead(){
+  postRead() {
     postArticleRead({
       id: this.props.match.params.id,
     }).then((res) => {
@@ -66,7 +99,7 @@ export default class ArticleDetail extends React.Component {
       });
     });
   }
-  init(){
+  init() {
     getArticle({
       id: this.props.match.params.id,
     }).then((res) => {
@@ -75,14 +108,14 @@ export default class ArticleDetail extends React.Component {
       });
     });
   }
-  _like(){
+  _like() {
     postArticleLike({
       id: this.props.match.params.id,
-    }).then(res=>{
+    }).then((res) => {
       this.setState({
         detail: res.data.article,
       });
       console.log(res);
-    })
+    });
   }
 }
